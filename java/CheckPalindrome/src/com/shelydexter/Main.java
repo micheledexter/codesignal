4ee8c2ab55ce7f98b6a7e10cc16bce89eaaf4cd6
Michele <micheleannedexter@gmail.com>
2018-11-07 14:10:46 -0600

    'checkPalindrome' answer and testing

---
+++ java/CheckPalindrome/src/com/shelydexter/Main.java
@@ -0,0 +1,65 @@
+package com.shelydexter;
+
+/*
+Given the string, check if it is a palindrome.
+
+Example:
+- For `inputString = "aabaa"`, the output should be
+`checkPalindrome(inputString) = true`;
+- For `inputString = "abac"`, the output should be
+`checkPalindrome(inputString) = false`;
+- For `inputString = "a"`, the output should be
+`checkPalindrome(inputString) = true`.
+
+Input/Output:
+- [execution time limit] 3 seconds (java)
+- [input] string inputString
+    A non-empty string consisting of lowercase characters.
+    Guaranteed constraints:
+    `1 ≤ inputString.length ≤ 10^5`.
+- [output] boolean
+    - `true` if `inputString` is a palindrome, `false` otherwise.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        test("aabaa", true, true);
+        test("abac", false, true);
+        test("a", true, true);
+        test("az", false, true);
+        test("abacaba", true, true);
+        test("z", true, true);
+        test("aaabaaaa", false, true);
+        test("zzzazzazz", false, true);
+        test("hlbeeykoqqqqokyeeblh", true, true);
+        test("hlbeeykoqqqokyeeblh", true, true);
+    }
+
+    // My answer
+    public static boolean checkPalindrome(String inputString) {
+        String[] stringArray = inputString.toLowerCase().split("\\s*[^A-Za-z0-9]\\s*");
+        String forwards = String.join("", stringArray);
+        String backwards = new StringBuilder(forwards).reverse().toString();
+        if (forwards.equals(backwards)) {
+            return true;
+        }
+        return false;
+    }
+
+    // Test method
+    public static boolean test(String inputString, boolean expected, boolean verbose) {
+        if (verbose) {
+            System.out.println("Input: inputString = " + inputString +
+                    "\nOutput: " + checkPalindrome(inputString) +
+                    "\nExpected: " + expected);
+            System.out.print("Result: ");
+        }
+        if (checkPalindrome(inputString) == expected) {
+            if (verbose) System.out.println("SUCCESSFUL\n");
+            return true;
+        }
+        if (verbose) System.out.println("!--- FAILED ---!");
+        return false;
+    }
+}
