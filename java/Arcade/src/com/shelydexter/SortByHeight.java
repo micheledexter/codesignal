98cf8646ac7b808dd0553d959a0cf69c8464432c
Michele <micheleannedexter@gmail.com>
2018-11-20 15:07:09 -0600

    'sortByHeight' tests

--- java/Arcade/src/com/shelydexter/SortByHeight.java
+++ java/Arcade/src/com/shelydexter/SortByHeight.java
@@ -26,7 +26,10 @@ import java.util.Arrays;
 public class SortByHeight {
 
     public static void main(String[] args) {
-
+        test(new int[] {-1, 150, 190, 170, -1, -1, 160, 180}, new int[] {-1, 150, 160, 170, -1, -1, 180, 190}, true);
+        test(new int[] {-1, -1, -1, -1, -1}, new int[] {-1, -1, -1, -1, -1}, true);
+        test(new int[] {-1}, new int[] {-1}, true);
+        test(new int[] {4, 2, 9, 11, 2, 16}, new int[] {2, 2, 4, 9, 11, 16}, true);
     }
 
     private static int[] sortByHeight(int[] a) {
