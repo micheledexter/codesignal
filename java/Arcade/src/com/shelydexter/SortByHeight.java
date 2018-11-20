b517ff96ba403cd643c731895dcdde64315199c1
Michele <micheleannedexter@gmail.com>
2018-11-20 14:59:37 -0600

    'sortByHeight' setup

---
+++ java/Arcade/src/com/shelydexter/SortByHeight.java
@@ -0,0 +1,50 @@
+package com.shelydexter;
+
+/*
+Some people are standing in a row in a park. There are trees between them which
+cannot be moved. Your task is to rearrange the people by their heights in a
+non-descending order without moving the trees. People can be very tall!
+
+Example:
+For `a = [-1, 150, 190, 170, -1, -1, 160, 180]`, the output should be
+`sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190]`.
+
+Input/Output:
+- [execution time limit] 3 seconds (java)
+- [input array.integer a
+    if `a[i] = -1`, then the `ith` position is occupied by a tree. Otherwise
+    `a[i]` is the height of a person standing in the `ith` position.
+    Guaranteed constraints:
+    `1 ≤ a.length ≤ 1000`,
+    `-1 ≤ a[i] ≤ 1000`.
+- [output] array.integer
+    - Sorted array `a` with all the trees untouched.
+ */
+
+import java.util.Arrays;
+
+public class SortByHeight {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int[] sortByHeight(int[] a) {
+
+    }
+
+    private static boolean test(int[] a, int[] expected, boolean verbose) {
+        if (verbose) {
+            System.out.println("Input: a = " + Arrays.toString(a) +
+                    "\nOutput: " + Arrays.toString(sortByHeight(a)) +
+                    "\nExpected: " + Arrays.toString(expected));
+            System.out.print("Result: ");
+        }
+        if (sortByHeight(a) == expected) {
+            if (verbose) System.out.println("SUCCESSFUL\n");
+            return true;
+        }
+        if (verbose) System.out.println("!--- FAILED ---!\n");
+        return false;
+    }
+}
