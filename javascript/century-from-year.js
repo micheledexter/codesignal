204202d6d7d603860d490379276530672e7592e2
Michele <micheleannedexter@gmail.com>
2018-12-02 14:31:52 -0600

    add instructions

--- javascript/century-from-year.js
+++ javascript/century-from-year.js
@@ -1,3 +1,22 @@
+/*
+Given a year, return the century it is in. The first century spans from the
+year 1 up to and including the year 100, the second - from the year 101 up to
+and including the year 200, etc.
+
+Example:
+- For `year = 1905`, the output should be `centuryFromYear(year) = 20`;
+- For `year = 1700`, the output should be `centuryFromYear(year) = 17`.
+
+Input/Output:
+- [execution time limit] 4 seconds (js)
+- [input] integer year
+    A positive integer, designating the year.
+    Guaranteed constraints:
+    `1 ≤ year ≤ 2005`.
+- [output] integer
+    - The number of the century the year is in.
+ */
+
 // My answer
 const centuryFromYear = year => {
   return Math.floor(((year - 1) / 100) + 1);
